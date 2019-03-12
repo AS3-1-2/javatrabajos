bddaa6ee42eaa6d55711e516a0bcdfecc2e9bf3e
AS3-1-02 <AS3-1-02@uni.lan>
2019-03-12 12:44:07 +0100

    a

--- nodejs/ejemplo01/ejemplo_formulario.js
+++ nodejs/ejemplo01/ejemplo_formulario.js
@@ -8,19 +8,16 @@ y = 6;
 
 z = x + y;
 
-if (z > 10) {
-    greeting = "es mayor que 10";
-} else {
-    if (z<10) {
-        greeting = "es menor que 10";
-    } else {
-        greeting = "es igual a 10"
-    }
-}
+
 // si la suma es > 10. Print "es mayor que 10"
 // si no: print "es menor que 10"
 // si = 10: "es igual a 10"
 // pista: usa "if / else"
 
-console.log(z)
+// console.log(z)
 
+
+if (z > 10) {
+    //true
+    console.log("es mayor que 10")
+}
