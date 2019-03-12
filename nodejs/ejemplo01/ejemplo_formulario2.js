41f9b1a6c69bcb3fb42b6c028a9371e35eb1e88a
AS3-1-02 <AS3-1-02@uni.lan>
2019-03-12 13:07:05 +0100

    a

---
+++ nodejs/ejemplo01/ejemplo_formulario2.js
@@ -0,0 +1,31 @@
+/*
+ejemplo 01
+*/
+
+//https://www.npmjs.com/package/http
+x = 5;
+y = 5;
+
+z = x + y;
+
+z = Math.floor(Math.random() * 51)
+// si la suma es > 10. Print "es mayor que 10"
+// si no: print "es menor que 10"
+// si = 10: "es igual a 10"
+// pista: usa "if / else"
+
+console.log("El valor de z es: " + z)
+
+
+
+
+// https://www.w3schools.com/js/js_if_else.asp
+if (z > 10) {
+    //true
+    console.log("es mayor que 10s")
+} else if (z < 10) {
+    //true
+    console.log("es menor que 10")
+} else {
+    console.log("es igual a 10")
+}
