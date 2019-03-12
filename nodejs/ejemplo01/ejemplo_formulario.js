47a1cdecd078b21b8f939b4134521e754dab1134
AS3-1-02 <AS3-1-02@uni.lan>
2019-03-12 12:37:15 +0100

    a

--- nodejs/ejemplo01/ejemplo_formulario.js
+++ nodejs/ejemplo01/ejemplo_formulario.js
@@ -4,10 +4,19 @@ ejemplo 01
 
 //https://www.npmjs.com/package/http
 x = 5;
-y = 1;
+y = 6;
 
 z = x + y;
 
+if (z > 10) {
+    greeting = "es mayor que 10";
+} else {
+    if (z<10) {
+        greeting = "es menor que 10";
+    } else {
+        greeting = "es igual a 10"
+    }
+}
 // si la suma es > 10. Print "es mayor que 10"
 // si no: print "es menor que 10"
 // si = 10: "es igual a 10"
