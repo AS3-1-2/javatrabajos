735bacffaaa05960eb7b8bf9a805ceedd72e09cb
AS3-1-02 <AS3-1-02@uni.lan>
2019-04-02 12:56:24 +0200

    a

--- nodejs/ejemplo02/app.js
+++ nodejs/ejemplo02/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send(html_prueba);
+  res.send(C:\xampp\htdocs\javatrabajos\nodejs\ejemplo02\unoo.html);
 });
 
 app.listen(3000, function () {
