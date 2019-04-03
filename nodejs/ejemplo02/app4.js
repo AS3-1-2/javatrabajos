ea3af112c9debbc80387d4dd16ee2a55484fcd99
AS3-1-02 <AS3-1-02@uni.lan>
2019-04-03 11:12:41 +0200

    a

--- nodejs/ejemplo02/app4.js
+++ nodejs/ejemplo02/app4.js
@@ -11,20 +11,20 @@ var express = require('express');
 var app = express();
 
 tiempo = [
-  r[0]= [{dia:"Miércoles,1", cielo:"lluvia", temperatura:12}],
+  {dia:"Miércoles,1", cielo:"lluvia", temperatura:12},
   {dia:"Jueves,2", cielo:"Nublado", temperatura:20},
-  r[1]= [{dia:"Viernes,3", cielo:"lluvia", temperatura:23}],
+  {dia:"Viernes,3", cielo:"lluvia", temperatura:23},
   {dia:"Sábado,4", cielo:"sol", temperatura:10},
-  r[2]= [{dia:"Domingo,5", cielo:"lluvia", temperatura:5}],
-  {dia:"lunes,6", cielo:"Nublado", temperatura:0}
-  {dia:"Martes,7", cielo:"sol", temperatura:25}
+  {dia:"Domingo,5", cielo:"lluvia", temperatura:5},
+  {dia:"lunes,6", cielo:"Nublado", temperatura:0},
+  {dia:"Martes,7", cielo:"sol", temperatura:25},
   {dia:"Miércoles,8", cielo:"sol", temperatura:12},
-  r[3]= [{dia:"Jueves,9", cielo:"lluvia", temperatura:9}],
+  {dia:"Jueves,9", cielo:"lluvia", temperatura:9},
   {dia:"Viernes,10", cielo:"Nublado", temperatura:16},
   {dia:"Sábado,11", cielo:"sol", temperatura:21},
   {dia:"Domingo,12", cielo:"sol", temperatura:18},
-  {dia:"lunes,13", cielo:"Nublado", temperatura:14}
-  r[4]= [{dia:"Martes,14", cielo:"lluvia", temperatura:11}]
+  {dia:"lunes,13", cielo:"Nublado", temperatura:14},
+  {dia:"Martes,14", cielo:"lluvia", temperatura:11}
 ]
 
 app.get('/', function (req, res) {
@@ -34,10 +34,10 @@ app.get('/', function (req, res) {
   //console.log("Días lluviosos:")
   for (var i = 0; i < tiempo.length; i++) { 
       //console.log(i)
-      if (tiempo[i].cielo == "lluvia") {
+      if (tiempo[i].tiempo == "lluvia") {
           //console.log(tiempo[i].dia + " " + tiempo[i].cielo)
           //console.log(tiempo[i].dia)
-          r[r.length] ='tiempo[i].dia'
+          r[r.length]=tiempo[i].dia
       }
   }
   res.send(r);
