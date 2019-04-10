41e94a4c31a617ba986ef8d45a55e6ffffd14982
AS3-1-02 <AS3-1-02@uni.lan>
2019-04-10 10:19:20 +0200

    a

--- nodejs/ejemplo02/app4.js
+++ nodejs/ejemplo02/app4.js
@@ -9,29 +9,26 @@ nube: http://www.euskalmet.euskadi.eus/appcont/meteorologia/meteodat/images/12.p
 
 var express = require('express');
 var app = express();
+// 
+app.use(express.static('public'))
+//app.use(express.static('.')) //segurtasun arazoa!!
 
 tiempo = [
-  {dia:"Miércoles,1", cielo:"lluvia", temperatura:12},
-  {dia:"Jueves,2", cielo:"Nublado", temperatura:20},
-  {dia:"Viernes,3", cielo:"lluvia", temperatura:23},
-  {dia:"Sábado,4", cielo:"sol", temperatura:10},
-  {dia:"Domingo,5", cielo:"lluvia", temperatura:5},
-  {dia:"lunes,6", cielo:"Nublado", temperatura:0},
-  {dia:"Martes,7", cielo:"sol", temperatura:25},
-  {dia:"Miércoles,8", cielo:"sol", temperatura:12},
-  {dia:"Jueves,9", cielo:"lluvia", temperatura:9},
-  {dia:"Viernes,10", cielo:"Nublado", temperatura:16},
-  {dia:"Sábado,11", cielo:"sol", temperatura:21},
-  {dia:"Domingo,12", cielo:"sol", temperatura:18},
-  {dia:"lunes,13", cielo:"Nublado", temperatura:14},
-  {dia:"Martes,14", cielo:"lluvia", temperatura:11}
+  {dia:"Miércoles", cielo:"lluvia", temperatura:12},
+  {dia:"Jueves", cielo:"Nublado", temperatura:20},
+  {dia:"Viernes", cielo:"Nublado", temperatura:23},
+  {dia:"Sábado", cielo:"sol", temperatura:10},
+  {dia:"Domingo", cielo:"lluvia", temperatura:5},
+  {dia:"lunes", cielo:"Nublado", temperatura:0}
 ]
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + 'euskalmet-html')
+app.get('/',function(req,res) {
+  //res.sendFile(__dirname + '/tiempo.html');
+  res.redirect('tiempo.html');
 });
 
-  
+app.get('/datuak', function (req, res) {
+
   var r = [];
   //console.log("Días lluviosos:")
   for (var i = 0; i < tiempo.length; i++) { 
@@ -39,12 +36,13 @@ app.get('/', function (req, res) {
       if (tiempo[i].cielo == "lluvia") {
           //console.log(tiempo[i].dia + " " + tiempo[i].cielo)
           //console.log(tiempo[i].dia)
-          r[r.length]=tiempo[i].dia
+          //r[r.length] = tiempo[i].dia
+          r.push(tiempo[i].dia)
       }
   }
+  
   res.send(r);
 });
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
